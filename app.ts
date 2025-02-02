cd4a86dec90ab559fda26ebdcbd8bd6339e950d3
Tully-L <GitHub 2332486893@qq.com>
2025-02-02 16:49:54 +0800

    fix version0(remove pages copy)

--- app.ts
+++ app.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /// <reference path="./typings/index.d.ts" />
 
 App<IAppOption>({
@@ -17,24 +16,4 @@ App<IAppOption>({
         },
       })
     },
-=======
-/// <reference path="./typings/index.d.ts" />
-
-App<IAppOption>({
-    globalData: {},
-    onLaunch() {
-      // 展示本地存储能力
-      const logs = wx.getStorageSync('logs') || []
-      logs.unshift(Date.now())
-      wx.setStorageSync('logs', logs)
-  
-      // 登录
-      wx.login({
-        success: res => {
-          console.log(res.code)
-          // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        },
-      })
-    },
->>>>>>> 30252565cb8ed3b276ff006e4bc3e9781cd83b84
   })
